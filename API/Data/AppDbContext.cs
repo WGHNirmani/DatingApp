330dede1107d48fd1915ea3c8dc485daed216220
WGHNirmani <hirunikanirmani26@gmail.com>
2025-08-21 21:18:06 +0530

    End of section 8

--- API/Data/AppDbContext.cs
+++ API/Data/AppDbContext.cs
@@ -4,7 +4,10 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Data;
 
-public class AppDbContext(DbContextOptions options): DbContext(options)
+public class AppDbContext(DbContextOptions options) : DbContext(options)
 {
       public DbSet<AppUser> Users { get; set; }
+      public DbSet<Member> Members { get; set; }
+
+      public DbSet<Photo> Photos { get; set; }
 }
--- API/Program.cs
+++ API/Program.cs
@@ -18,6 +18,7 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
 });
 builder.Services.AddCors();
 builder.Services.AddScoped<ITokenService, TokenServices>();
+builder.Services.AddScoped<IMemberRepository, MemberRepository>();
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer(options =>
     {
@@ -43,6 +44,21 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+
+using var scope = app.Services.CreateAsyncScope();
+var services = scope.ServiceProvider;
+
+try
+{
+    var context = services.GetRequiredService<AppDbContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedUsers(context);
+}
+catch (Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error occured during migration");
+}
 
 app.Run();
 
